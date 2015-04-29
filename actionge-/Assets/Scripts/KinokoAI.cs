fe113ab87e1eb65699021d66d3a8aa4867b35f2b
Rei kobayashi <mokkan21@yahoo.co.jp>
2015-04-29 22:29:19 +0900

    敵AIのスクリプト修正 ステージ若干変更

--- actionge-/Assets/Scripts/KinokoAI.cs
+++ actionge-/Assets/Scripts/KinokoAI.cs
@@ -9,11 +9,23 @@ public class KinokoAI : MonoBehaviour {
 	private float speed;
 	
 	//方向転換のtimer
-	public float time;
-	
+	//public float time;
+
+	//前に進むときのフラグ。bool型
+	[SerializeField]
+	private bool forward;
+
+	[SerializeField]
+	private Vector3 startPos;
+	//うごく範囲の指定
+	[SerializeField]
+	public float moveX;
+
 	// Use this for initialization
 	void Start () {
-		time = 0;
+		//time = 0;
+		forward = true;
+		startPos = this.gameObject.transform.position;
 		anim = GetComponent<Animator> ();
 		speed = anim.GetFloat ("Speed");
 	}
@@ -27,11 +39,24 @@ public class KinokoAI : MonoBehaviour {
 
 	//移動関数
 	void Move(){
-		time += Time.deltaTime;
-		this.transform.localPosition += transform.forward * speed * Time.deltaTime;
-		ChangeDirection (5.0f);
-	}
+		if (this.transform.position.x > startPos.x + moveX) {
+			transform.rotation = Quaternion.AngleAxis (90f, Vector3.up);
+			forward = false;
+		}
+		if (this.transform.position.x < startPos.x - moveX) {
+			transform.rotation = Quaternion.AngleAxis (-90f, Vector3.up);
+			forward = true;
+		}
 
+		if(forward)
+			this.transform.localPosition += transform.forward * speed * Time.deltaTime;
+		else 
+			this.transform.localPosition -= transform.forward * speed * Time.deltaTime;
+
+
+		//ChangeDirection (5.0f);
+	}
+	/*
 	//向きを変える関数
 	void ChangeDirection(float t){
 		if (t < time) {
@@ -39,7 +64,7 @@ public class KinokoAI : MonoBehaviour {
 			time = 0;
 		}
 	}
-
+*/
 	//プレイヤーにあたったときの判定
 	void OnCollisionEnter(Collision col){
 		if (col.gameObject.tag == "Player") {
