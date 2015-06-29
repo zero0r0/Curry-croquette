e990c012dbee4ee33cf315e31149e20e0f279187
Rei kobayashi <mokkan21@yahoo.co.jp>
2015-06-29 19:55:23 +0900

    no message

---
+++ actionge-/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		Destroy (this.gameObject);
+	}
+}
